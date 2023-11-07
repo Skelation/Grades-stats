4ca3186fdbaab57083a822a1820d986dd50394c5
Heribio <ethan.levratti@icloud.com>
2023-11-07 19:34:52 +0100

    Updated the json file format

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ os.chdir(script_directory)
 
 # Get user input (To change to a custom tkinter GUI)
 grade = input("What's your grade? ")
+name = input("Name ? ")
 
 # Generate a timestamp to use as a unique identifier
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -26,7 +27,21 @@ if os.path.exists("data.json"):
         data = json.load(file)
 else:
     # If the file doesn't exist, initialize an empty dictionary
-    data = {}
+    data = {
+
+    }
+
+# Count the existing entries and assign the next integer key
+next_key = str(len(data) + 1)
+
+# Define the new data entry with the next integer key
+json_input = {
+    next_key: {
+        "Grade": grade,
+        "Timestamp": timestamp,
+        "Name": name
+    }
+}
 
 # Add the new data entry to the dictionary
 data.update(json_input)
