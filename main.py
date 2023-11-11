6f24525f55bcae4e48f2f5ac78d9f79f48aad394
Heribio <ethan.levratti@icloud.com>
2023-11-11 20:00:20 +0100

    The whole program is now in one file

--- main.py
+++ main.py
@@ -1,19 +1,106 @@
-import os
-import json
+import os, json, webbrowser
 from datetime import datetime
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-import webbrowser
 import customtkinter as ctk
+from tkinter import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import matplotlib
 
-class ToplevelWindow(ctk.CTkToplevel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.geometry("400x300")
+class ToplevelWindow(tk.Toplevel):
+    def __init__(self, window):
 
-        self.label = ctk.CTkLabel(self, text="ToplevelWindow")
-        self.label.pack(padx=20, pady=20)
+        # Load data from data.json
+        with open("data.json", "r") as file:
+            data_from_file = json.load(file)
+
+        # Extract grades from the data
+        grades = [int(entry.get("Grade", 0)) for entry in data_from_file.values() if entry.get("Grade")]
+        names = [entry.get("Name", "") for entry in data_from_file.values() if entry.get("Name")]
+
+        # Set the Matplotlib backend to "TkAgg"
+        matplotlib.use("TkAgg")
+
+        self.master = window
+        # Create a tkinter window
+        window = tk.Tk()
+        window.iconbitmap("./assets/sheet.ico")
+        window.minsize(830, 500)
+        window.geometry("+580+100")
+        window.title("Grades")
+        window.config(bg="#242324")
+        window.attributes('-alpha', 0.95)
+        window.maxsize(830, 500)
+
+        # Create the Grades Frame
+        grade_frame = ctk.CTkScrollableFrame(window, fg_color="#2b2b2b", 
+                                            orientation= "vertical", 
+                                            scrollbar_button_color="#696968", 
+                                            label_text="Grades", 
+                                            label_font= ('Poppins bold', 20), 
+                                            label_anchor="w", 
+                                            label_text_color="white", 
+                                            label_fg_color="#2b2b2b")
+        grade_frame.grid(row=1, column=0, padx=10, sticky="nsew", pady=10)
+
+        for key, entry in data_from_file.items():
+            grade = entry.get("Grade")
+            name = entry.get("Name")
+            if grade:
+                grade_label = tk.Label(grade_frame, text=f"{name}: {grade}%", bg="#2b2b2b", fg="white", font=("Poppins", 12))
+                grade_label.pack(anchor="w", padx=5, pady=5)
+
+        # Create the Graph frame
+        Linear_graph_frame = tk.Frame(window, bg="#2b2b2b")
+        Linear_graph_frame.grid(row=1, column=1, padx=10, sticky="nsew", pady= 10)
+
+        # Set row and column weights to control resizing
+        window.grid_rowconfigure(1, weight=1)
+        window.grid_columnconfigure(0, weight=1)
+
+        # Create a line graph of the grades using Matplotlib
+        fig, ax = plt.subplots(figsize=(6, 4))
+        fig.set_facecolor("#2b2b2b")
+        ax.set_facecolor("#2b2b2b")
+        ax.plot(range(1, len(grades) + 1), grades, marker='o', linestyle='-')
+
+        # Set the color of x and y labels to white
+        ax.set_xlabel("Grades", color = "white")
+        ax.set_ylabel("Grade Value", color = "white")
+        ax.set_title("Grades Overview", color = "white")
+        ax.grid(True)
+
+        # Set the color of tick labels on the x and y axes to white
+        ax.tick_params(axis='x', labelcolor='white')
+        ax.tick_params(axis='y', labelcolor='white')
+
+        ax.grid(color='white')
+
+        # Update the line and marker colors to make them more visible on a black background
+        ax.spines['bottom'].set_color('white')
+        ax.spines['top'].set_color('white')
+        ax.spines['left'].set_color('white')
+        ax.spines['right'].set_color('white')
+        ax.xaxis.label.set_color('white')
+        ax.yaxis.label.set_color('white')
+
+        # Annotate data points with names
+        for i, name in enumerate(names):
+            ax.annotate(name, (i + 1, grades[i]), xytext=(1, 5), textcoords='offset points', color="limegreen")
+
+        # Embed the Matplotlib figure in the tkinter window
+        canvas = FigureCanvasTkAgg(plt.gcf(), master=Linear_graph_frame)
+        canvas.get_tk_widget().pack(fill="both", expand=True)
+
+        # Bind the closing function to the window's close button
+        window.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def on_closing(self):
+        # This function will be called when the window is closed
+        self.master.quit()  # Quit the main loop
+        self.master.destroy()
 
 class GradeGrapherApp:
     def __init__(self, master):
@@ -22,8 +109,6 @@ class GradeGrapherApp:
         master.geometry("600x270")
         master.iconbitmap("./assets/sheet.ico")
         master.minsize(600, 270)
-        master.maxsize(600, 270)
-
         self.setup_gui()
 
     def setup_gui(self):
@@ -75,7 +160,7 @@ class GradeGrapherApp:
         submit_button = ctk.CTkButton(self.master, text="Submit", command=self.submit_values)
         submit_button.grid(row=4, column=0, columnspan=3, pady=10)
 
-        open_toplevel_button = ctk.CTkButton(self.master, text="Open Graph", command=self.open_toplevel, image=)
+        open_toplevel_button = ctk.CTkButton(self.master, command=self.open_toplevel, text="Open Graph")
         open_toplevel_button.grid(row=4, column=1, columnspan=1, pady=10)
 
         self.master.configure(bg='#242324')
